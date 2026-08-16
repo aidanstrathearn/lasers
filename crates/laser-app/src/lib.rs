0c2e8e3871fb6506c9b776c5bf2edd7db07074e0
aidanstrathearn <aidanstrathearn@gmail.com>
2026-08-17 01:18:21 +1000

    playing with egui + working out good design

--- crates/laser-app/src/lib.rs
+++ crates/laser-app/src/lib.rs
@@ -1,11 +1,19 @@
 use eframe::egui;
-use eframe::egui::Ui;
+use eframe::egui::{Response, Ui};
 use egui_plot::{Legend, Line, Plot, PlotPoints};
 use laser_solver::lase::{FibreParams, GratingProfile, GridPoints, Pump, dfb_solve};
 use laser_solver::rootfind::{Newton1dConfig, RootFindError};
 use std::sync::mpsc::Receiver;
 use std::sync::{Arc, mpsc};
 use std::thread;
+
+pub fn f64_slider(handle: &mut f64, name: &str, min: f64, max: f64, step: f64, ui: &mut Ui) -> Response {
+    ui.horizontal(|ui| {
+        ui.label(name);
+        ui.add(egui::Slider::new(handle, min..=max).step_by(step))
+    })
+    .inner
+}
 
 #[derive(PartialEq)]
 pub enum View {
@@ -15,8 +23,67 @@ pub enum View {
 
 type Points = Vec<[f64; 2]>;
 
+pub struct ProfilePlot {
+    pump: Pump,
+    fibre_params: FibreParams,
+    grid_points: GridPoints,
+    grating: GratingProfile,
+    pending: Option<Receiver<[Points; 4]>>,
+    result: Option<[Points; 4]>
+}
+
+impl ProfilePlot {
+    #[cfg(not(target_arch = "wasm32"))]
+    fn start_compute(&mut self, ctx: egui::Context) {
+        let full_profile = true;
+        let nc = Newton1dConfig {
+            tolerance: 1e-8f64,
+            max_iters: 100usize,
+            initial: self.pump.forward,
+            dx: 1e-6,
+        };
+        let pump = self.pump;
+        let fibre_params = self.fibre_params;
+        let grid_points = self.grid_points;
+        let grating = self.grating;
+        let compute_fn = move || {
+            let result = dfb_solve(pump, fibre_params, grid_points, grating, full_profile, nc)?;
+            Ok([result.plotpoints("sgnl_f"),
+                result.plotpoints("sgnl_b"),
+                result.plotpoints("pump_f"),
+                result.plotpoints("pump_f")])
+        };
+
+        let (tx, rx) = mpsc::channel();
+
+        self.pending = Some(rx);
+
+        thread::spawn(move || {
+            //thread::sleep(Duration::from_millis(100));
+            let points: Result<[Points;4], RootFindError> = compute_fn();
+            let _ = tx.send(points.unwrap());
+            ctx.request_repaint();
+        });
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    fn fetch_result(&mut self) {
+        // &self.pending: &Option<..>, and rust treats LHS as &Some(ref rx) -- generally derefs through pattern matching
+        // e.g. (a, b) = &my_tuple derefs the outer tuple and makes a,b: &...
+        // using self.pending.as_ref(): Option<&..> would mean nothing implicit happens
+        if let Some(rx) = &self.pending {
+            if let Ok(points) = rx.try_recv() {
+                self.result = Some(points);
+                self.pending = None;
+            }
+        }
+    }
+}
+
+
 pub struct LaserApp {
     view: View,
+    pump: Pump,
     fibre_params: FibreParams,
     grid_points: GridPoints,
     grating: GratingProfile,
@@ -36,6 +103,11 @@ impl Default for LaserApp {
             length: 10.0,
         };
 
+        let pu = Pump {
+            forward: 100.0,
+            backward: 0.0,
+        };
+
         let kp = GratingProfile {
             kappa_max: 1.0,
             pi_shift_position: 0.45,
@@ -45,6 +117,7 @@ impl Default for LaserApp {
 
         Self {
             view: View::Sin,
+            pump: pu,
             fibre_params: fp,
             grating: kp,
             grid_points: gp,
@@ -63,20 +136,15 @@ impl LaserApp {
     }
 
     pub fn compute_and_plot(&mut self, ui: &mut Ui) -> Result<(), RootFindError> {
-        let pu = Pump {
-            forward: 100.0,
-            backward: 0.0,
-        };
-
         let full_profile = true;
         let nc = Newton1dConfig {
             tolerance: 1e-8f64,
             max_iters: 100usize,
-            initial: pu.forward,
+            initial: self.pump.forward,
             dx: 1e-6,
         };
         let result = dfb_solve(
-            pu,
+            self.pump,
             self.fibre_params,
             self.grid_points,
             self.grating,
@@ -107,7 +175,7 @@ impl LaserApp {
     pub fn param_sliders(&mut self, ui: &mut Ui) {
         ui.horizontal(|ui| {
             ui.label("pi pos");
-            let response = ui.add(
+            ui.add(
                 egui::Slider::new(&mut self.grating.pi_shift_position, 0.01..=1.0).step_by(0.01),
             );
             ui.separator();
@@ -119,10 +187,18 @@ impl LaserApp {
             ui.label("length");
             ui.add(egui::Slider::new(&mut self.fibre_params.length, 0.5..=20.0).step_by(0.01));
             ui.separator();
-
+        });
+        ui.horizontal(|ui| {
             ui.label("grid points");
             ui.add(egui::Slider::new(&mut self.grid_points.0, 10..=1000).step_by(0.01));
             ui.separator();
+
+            ui.label("pump");
+            ui.add(egui::Slider::new(&mut self.pump.forward, 0.1..=200.0).step_by(0.01));
+            ui.separator();
+
+            f64_slider(&mut self.fibre_params.pump_em, "pump_em", 0.0, 10.0, 0.01, ui);
+            f64_slider(&mut self.fibre_params.lifetime, "lifetime", 0.01, 10.0, 0.01, ui);
         });
     }
 
@@ -133,51 +209,7 @@ impl LaserApp {
         });
     }
 
-    #[cfg(not(target_arch = "wasm32"))]
-    fn start_compute(&mut self, ctx: egui::Context) {
-        let pu = Pump {
-            forward: 100.0,
-            backward: 0.0,
-        };
-        let full_profile = true;
-        let nc = Newton1dConfig {
-            tolerance: 1e-8f64,
-            max_iters: 100usize,
-            initial: pu.forward,
-            dx: 1e-6,
-        };
-        let fibre_params = self.fibre_params;
-        let grid_points = self.grid_points;
-        let grating = self.grating;
-        let compute_fn = move || {
-            let result = dfb_solve(pu, fibre_params, grid_points, grating, full_profile, nc)?;
-            Ok(result.plotpoints("sgnl_f"))
-        };
 
-        let (tx, rx) = mpsc::channel();
-
-        self.pending = Some(rx);
-
-        thread::spawn(move || {
-            //thread::sleep(Duration::from_millis(100));
-            let points: Result<Points, RootFindError> = compute_fn();
-            let _ = tx.send(points.unwrap());
-            ctx.request_repaint();
-        });
-    }
-
-    #[cfg(not(target_arch = "wasm32"))]
-    fn fetch_result(&mut self) {
-        // &self.pending: &Option<..>, and rust treats LHS as &Some(ref rx) -- generally derefs through pattern matching
-        // e.g. (a, b) = &my_tuple derefs the outer tuple and makes a,b: &...
-        // using self.pending.as_ref(): Option<&..> would mean nothing implicit happens
-        if let Some(rx) = &self.pending {
-            if let Ok(points) = rx.try_recv() {
-                self.result = Some(points);
-                self.pending = None;
-            }
-        }
-    }
 }
 
 impl eframe::App for LaserApp {
